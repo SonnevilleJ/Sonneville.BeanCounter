7cecfabc102dd2efd89ced91aa52062191492ea9
John Sonneville <SonnevilleJ@gmail.com>
2017-01-12 23:04:25 -0600

    Hello World OWIN app running

---
+++ OwinWebApp/HelloController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace OwinWebApp
+{
+    public class HelloController : ApiController
+    {
+        public string Get(string id = "World")
+        {
+            return $"Hello, {id}!";
+        }
+    }
+}
---
+++ OwinWebApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Owin.Hosting;
+
+namespace OwinWebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string baseAddress = "http://localhost:1234/";
+
+            using (WebApp.Start<Startup>(baseAddress))
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync(baseAddress + "api/hello").Result;
+
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                Console.ReadLine();
+            }
+        }
+    }
+}
